4b468e362a12923a917df3bd8285eb41f4bf00e2
KastratiDren <drenkastrati33@gmail.com>
2024-11-18 10:57:44 +0100

    Add Validation Rules for Command Classes using FluentValidation

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,31 +1,46 @@
-﻿namespace Catalog.API.Products.UpdateProduct;
-
-public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : ICommand<UpdateProductResult>;
-public record UpdateProductResult(bool IsSuccess);
-
-internal class UpdateProductCommandHandler
-    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
-    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
     {
-        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
-
-        if (product is null)
+        public UpdateProductCommandValidator()
         {
-            throw new ProductNotFoundException();
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
         }
+    }
 
-        product.Name = command.Name;
-        product.Category = command.Category;
-        product.Description = command.Description;
-        product.ImageFile = command.ImageFile;
-        product.Price = command.Price;
+    internal class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
 
-        session.Update(product);
-        await session.SaveChangesAsync(cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
 
-        return new UpdateProductResult(true);
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
     }
 }
+
